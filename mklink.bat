052f1b17fbfcc19e809662454065fba7b000db59
Erwan DAYOT <pro.e.dayot@gmail.com>
2021-12-26 12:49:48 +0100

    mklink update

--- mklink.bat
+++ mklink.bat
@@ -1,3 +1,3 @@
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - Base"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - ConnectedOres"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base" "AirCubic - Base"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres" "AirCubic - ConnectedOres"
 PAUSE
