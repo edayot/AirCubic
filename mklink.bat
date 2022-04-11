f2861179c67b0046b5e8b2763369ac3e85c23a46
edayot <pro.e.dayot@gmail.com>
2022-04-11 22:26:47 +0200

    mklink

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,5 @@
 rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base"
 rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base" "D:\Documents\Python VS\AirCubic\AirCubic - Base"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres" "D:\Documents\Python VS\AirCubic\AirCubic - ConnectedOres"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - Base"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - ConnectedOres"
 PAUSE
