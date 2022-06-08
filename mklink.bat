35e2c3ad0839de245d441c63b8ea1dfd33f38a8c
edayot <pro.e.dayot@gmail.com>
2022-06-08 19:45:25 +0200

    fixes et compatibilité 1.19

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,5 @@
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base"
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - Base" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - Base"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\AirCubic - ConnectedOres" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - ConnectedOres"
+rd "C:\Users\erwan\AppData\Roaming\.instance\resourcepacks\AirCubic - Base"
+rd "C:\Users\erwan\AppData\Roaming\.instance\resourcepacks\AirCubic - ConnectedOres"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\resourcepacks\AirCubic - Base" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - Base"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\resourcepacks\AirCubic - ConnectedOres" "C:\Users\erwan\Documents\Dev\AirCubic\AirCubic - ConnectedOres"
 PAUSE
