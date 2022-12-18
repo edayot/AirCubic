f647106730af268ea02d948aa0b6390ce8220892
Erwan DAYOT <pro.e.dayot@gmail.com>
2022-12-18 17:00:26 +0100

    update links

---
+++ link.sh
@@ -0,0 +1,4 @@
+ln -s "/home/erwan/Documents/Dev/AirCubic/AirCubic - 1.8.9" "/home/erwan/.config/gdlauncher_next/resourcepacks/AirCubic - 1.8.9"
+ln -s "/home/erwan/Documents/Dev/AirCubic/AirCubic - Base" "/home/erwan/.config/gdlauncher_next/resourcepacks/AirCubic - Base"
+ln -s "/home/erwan/Documents/Dev/AirCubic/AirCubic - ConnectedOres" "/home/erwan/.config/gdlauncher_next/resourcepacks/AirCubic - ConnectedOres"
+
